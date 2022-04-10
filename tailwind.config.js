8268f5a347c75bf71592ce8445b8361c5aae3817
Kwaku Owusu <owusuk2004@outlook.com>
2022-04-10 19:59:16 +0100

    Completed the fully responsive landing page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'myblue': '#21243D',
+        'resumered': '#FF6464',
+        'bpostbg': '#EDF7FA',
+        'workbtnclr': '#142850',
+      },
+    },
   },
   plugins: [],
 }
